75a883390cef56f6a5bff5fb0902e38aaf0f74c6
Kevin <127465390+Anjayhayuk@users.noreply.github.com>
2023-04-16 22:44:07 +0700

    Add files via upload

---
+++ object/penulisan_object.js
@@ -0,0 +1,35 @@
+// let personArr= ["John","Bjon","Male",25];
+
+// console.log(personArr[0])
+
+// let personObj={
+//     firstname: "John",
+//     lastname: "Bjon",
+//     gender: "Male",
+//     age: 25
+// }
+// console.log(personObj.firstname)
+
+// 1 Game
+// let game={
+//     nama: "Genshin Impact",
+//     kategori: "RPG Open World",
+//     developer: "Mihoyo",
+// }
+// console.log(game)
+
+// 2 Motor
+// let motor={
+//     nama: "Yamaha",
+//     tipe: "Mio",
+//     kecepatan: "125 CC",
+// }
+// console.log(motor)
+
+// 3 Kereta
+let kereta={
+    nama: "Kereta Api",
+    tipe: "MRT",
+    jumlah_gerbong: "12",
+}
+console.log(kereta)
